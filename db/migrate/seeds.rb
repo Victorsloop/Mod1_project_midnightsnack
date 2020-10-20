d3d1dfab2196415ced042da4446f3025bd269b07
Victorsloop <torres.victor136@gmail.com>
2020-10-20 11:12:19 -0400

    code into seed

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -1,3 +1,17 @@
 require 'pry'
 require 'rest-client' # let you actually make the requests to the URLs 
 require 'json'
+Snacker.destory_all
+Ingredient.destory_all
+FavoriteIngredient.destory_all
+EatenSnack.destory_all
+
+
+
+
+api_response = RestClient.get(##whichever is the best)
+api_data = JSON.parse(api_response)
+ 
+binding.pry 
+
+snacker.create(username:"Sean", password: "11111")
