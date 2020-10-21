4d547ccd37699c048868cfddcced912dc98e336a
sdelane2 <seanmartindelaney@gmail.com>
2020-10-21 12:52:18 -0400

    moved some files around

--- lib/models/cli_recipie_search.rb
+++ lib/models/cli_recipie_search.rb
@@ -1,3 +1,7 @@
+require 'rubygems'
+require 'recipe_puppy'
+
+
 def fridge_pantry
     puts "What ingredient did you find ? :"
   ingredient = gets.chomp
@@ -6,3 +10,20 @@ end
 
 def recipie_search
 end 
+
+# Simple Search
+# steakrecipes = RecipePuppy::Recipe.search_for("steak")
+# steakrecipes.get['results'].each do |recipe|
+#   puts recipe['title']
+#   puts recipe['href']
+#   puts recipe['ingredients']
+#   puts recipe['thumbnail']
+# end
+
+eggrecipes = RecipePuppy::Recipe.search_for("eggs, butter, salt")
+eggrecipes.get['results'].each do |recipe|
+    puts recipe['title']
+    puts recipe['ingredients']
+    puts "What recipe would you like to save?"
+    saved_recipe = gets.chomp
+end
