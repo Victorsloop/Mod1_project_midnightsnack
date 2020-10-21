31c5026815a852fbc39b12ff95fade4ed400eaac
Victorsloop <torres.victor136@gmail.com>
2020-10-21 11:49:22 -0400

    changed ing to rec

--- lib/models/ingredient.rb
+++ lib/models/ingredient.rb
@@ -1,11 +1,6 @@
 class Ingredient < ActiveRecord::Base
-<<<<<<< HEAD
     has_many :snackers, through: :favorite_recipies
     has_many :favorite_recipies
-=======
-    has_many :snackers, through: :favorite_ingredients
-    has_many :favorite_ingredients
     belongs_to :eaten_snacks
->>>>>>> 251f9efc32b7981dc4953a8a5baf59b077d3197e
     
 end
