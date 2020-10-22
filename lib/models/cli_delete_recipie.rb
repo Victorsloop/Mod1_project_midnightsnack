3b371cbae98072119a9e624bda86b19691e785ac
Victorsloop <torres.victor136@gmail.com>
2020-10-22 09:14:55 -0400

    synntax again

--- lib/models/cli_delete_recipie.rb
+++ lib/models/cli_delete_recipie.rb
@@ -1,12 +1,12 @@
 ### Basically have to call out the favorites , and allow them to choose which ones to delte 
-def display_faves(favorites)
+def display_faves(fav)
     puts "These are all your favorite recipes"
     favorites.each_with_index do |fave, i|
       recipe = Recipe.find(fave.recipe_id)
       puts "#{i+1}. #{recipe.name}" ## Listing them down 
     end
     print "Which recipe would you like to remove from your favorites? "
-    rec_num = gets.chomp.to_i
+    delete = gets.chomp.to_i
     puts
     recipe = Recipe.find(favorites[rec_num-1].recipe_id)
     print "Confirm deletion of #{recipe.name}? (y/n) "
